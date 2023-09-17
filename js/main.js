8fc9acf2ff74315af426130c1c4639b98a480b14
Allan <workofcode@gmail.com>
2023-09-17 22:55:45 +0100

    - basic display of star data
    - now displays an extremely basic 3d representation of the data
    - will add to this as I learn more ThreeJS

--- js/main.js
+++ js/main.js
@@ -1,44 +1,171 @@
-let camera, scene, renderer;
+//import * as THREE from 'three';
+//import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-init();
 
-function init() {
-  // Create a camera
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
-  camera.position.set(0, 0, 1000);
+// Setup ThreeJS scene and camera
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 2000 );
 
-  // Create a scene
-  scene = new THREE.Scene();
+// Axis helper helps me work in 3d space.
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
 
-  // Create a renderer
-  renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('map'), antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
+// Creater renderer and set configs.
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-  // Add event listeners for user interaction
-  document.addEventListener('mousemove', onDocumentMouseMove);
-  document.addEventListener('mousedown', onDocumentMouseDown);
-  window.addEventListener('resize', onWindowResize);
+// Controls for orbit cam
+//const controls = new OrbitControls( camera, renderer.domElement );
 
-  // Start the rendering loop
-  render();
+
+//----------------
+//  vars
+//----------------
+let starsData = [];
+let starsMeshData = [];
+const starTypes = ['Blue', 'Blue-White', 'Yellow','Orange','Red']
+const starColors = ['blue', 'white', 'yellow', 'orange', 'red']
+
+const ambientlight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientlight);
+
+
+camera.position.x = 0.5;
+camera.position.y = 0.5;
+camera.position.z = 0.5;
+
+
+getData()
+
+
+
+
+// --------
+// Functions
+// --------
+async function getData() {
+  console.log('loading data')
+  try {
+    const response = await fetch("data/star-map.json", {
+      method: "GET", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+
+
+    let data = await response.json() 
+    starsData = data.solarSystems;
+    console.log(starsData)
+    if(starsData){
+      for(let i = 0; i < starsData.length ; i++){
+        console.log('We have a star')
+        drawStar(starsData[i], i)
+      }
+    }
+    renderer.render(scene, camera);
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
 
-function render() {
-  requestAnimationFrame(render);
+function drawStar(star, index){
+  console.log(star)
+  const geometry = new THREE.SphereGeometry( 0.001, 20, 20 );
+  const material = new THREE.MeshBasicMaterial( { color: '#ffffff' , opacity: '0.03'} );
+  starsMeshData[index] = new THREE.Mesh( geometry, material );
+  starsMeshData[index].position.set(star.x, star.y, star.z)
+  scene.add( starsMeshData[index] );
 
-  // Update the camera and renderer
-  camera.lookAt(scene.position);
-  renderer.render(scene, camera);
+
+  /*
+
+  starsMeshData[i].position.x =stars[i].axisX
+  starsMeshData[i].position.y =stars[i].axisY
+  starsMeshData[i].position.z =stars[i].axisZ
+
+
+  scene.add( starsMeshData[i] );
+
+
+
+
+
+      const light = new THREE.PointLight( '#', 1, 100 );
+      light.position.set( 0, 0, 0 );
+      scene.add( light );
+
+
+   // lightObjects[i] = new THREE.PointLight( 0xff0000, 1, 100 );
+   // lightObjects[i].position.set( stars[i].axisX, stars[i].axisY, stars[i].axisZ );
+   // scene.add( lightObjects[i] );
+
+    //starObjects[i].set(Math.floor(Math.random() * 10), Math.floor(Math.random() * 10), Math.floor(Math.random() * 10))
+    scene.add( starObjects[i] );
+    */
 }
 
-function onDocumentMouseMove(event) {
-  // Handle mouse movement
+
+
+function generateStars(){
+  const starObjects = [];
+  const lightObjects = [];
+
+
+  for(let i = 0; i < stars.length; i++){
+    const geometry = new THREE.SphereGeometry( stars[i].size, 20, 20 );
+    const material = new THREE.MeshBasicMaterial( { color: stars[i].color , opacity: '0.03'} );
+    starObjects[i] = new THREE.Mesh( geometry, material );
+    starObjects[i].position.x =stars[i].axisX
+    starObjects[i].position.y =stars[i].axisY
+    starObjects[i].position.z =stars[i].axisZ
+
+
+      const light = new THREE.PointLight( '#', 1, 100 );
+      light.position.set( 0, 0, 0 );
+      scene.add( light );
+
+
+   // lightObjects[i] = new THREE.PointLight( 0xff0000, 1, 100 );
+   // lightObjects[i].position.set( stars[i].axisX, stars[i].axisY, stars[i].axisZ );
+   // scene.add( lightObjects[i] );
+
+    //starObjects[i].set(Math.floor(Math.random() * 10), Math.floor(Math.random() * 10), Math.floor(Math.random() * 10))
+    scene.add( starObjects[i] );
+  }
 }
 
-function onDocumentMouseDown(event) {
-  // Handle mouse clicks
+function autoZoom(){  
+ if(cameraZoomMode === 'out'){
+    if(camera.position.z <= 0.85){
+      camera.position.z += 0.001;
+      camera.position.x += 0.001;
+      camera.position.y += 0.001;
+    }else{
+      cameraZoomMode = 'in'
+    }
+  }else{
+    if(camera.position.z >= 0.5){
+      camera.position.z -= 0.001;
+      camera.position.x -= 0.001;
+      camera.position.y -= 0.001;
+    }else
+    {
+      cameraZoomMode = 'out'
+    }
+  }
 }
 
-function onWindowResize() {
-  // Handle window resizing
+
+let cameraZoomMode = 'out'
+// Animation 
+function animate() {	
+  requestAnimationFrame( animate );
+  autoZoom()
+  camera.lookAt(0,0,0)
+  //controls.update();
+	renderer.render( scene, camera );
 }
+animate();
